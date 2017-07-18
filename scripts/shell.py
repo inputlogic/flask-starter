b3ea6851c70321150d5c7f19402377388aa09469
Adriaan Mulder <adriaan.mulder@gmail.com>
2017-07-18 12:51:22 -0700

    Add models to the global namespace on `make-shell`.

--- scripts/shell.py
+++ scripts/shell.py
@@ -1,5 +1,15 @@
+import importlib
 from app import create_app
+import config
+
+def camel_case(string):
+    return ''.join([x.title() for x in string.split('_')])
+
+def load_models():
+    for name in config.MODELS:
+        module = importlib.import_module('app.models.{0}'.format(name))
+        model = camel_case(name)
+        globals()[model] = getattr(module, model)
 
 create_app()
-
-from app.models import *
+load_models()
